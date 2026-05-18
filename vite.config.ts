e71f62fef7e0af2e94740a741163af68e9e00a43
adilhasssan143-arch <adilhasssan143@gmail.com>
2026-05-18 13:20:13 +0500

    fixed base path and cleaned build

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/', // 🚀 User repository (adilhassan85.github.io) ke liye single slash hi perfect standard hai
 })
